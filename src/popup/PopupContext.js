dfc4864405b5b9a4aad2e8490241ec4948a9b960
Thermalight <alumite10@gmail.com>
2024-02-01 14:32:22 +0100

    add context to app

---
+++ src/popup/PopupContext.js
@@ -0,0 +1,26 @@
+import { createContext, useState } from 'react';
+
+const PopupContext = createContext();
+
+let initialState = {
+  cookiesClicked: 0,
+  goldenCookiesClicked: 0,
+  upgradesBought: 0,
+  buildingsBought: 0,
+  settings: {
+    autoCookieClicker: false,
+    autoGoldenCookieClicker: false,
+    autoUpgradeBuyer: false,
+    autoBuildingBuyer: false,
+    stockMarketHelper: false,
+  }
+};
+
+export const PopupContextProvider = ({ children }) => {
+  const [popupStore, setPopupStore] = useState(initialState);
+  return (
+    <PopupContext.Provider value={{popupStore, setPopupStore}}>
+      {children}
+    </PopupContext.Provider>
+  );
+}
